8a626538d116e457316c994b29033a3bb3d02331
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-06 16:48:21 -0500

    Updated product_controller params

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -21,7 +21,7 @@ class Admin::ProductsController < AdminController
 
   # POST /admin/products or /admin/products.json
   def create
-    @admin_product = Product.new(product_params)
+    @admin_product = Product.new(admin_product_params)
 
     respond_to do |format|
       if @admin_product.save
@@ -36,14 +36,16 @@ class Admin::ProductsController < AdminController
 
   # PATCH/PUT /admin/products/1 or /admin/products/1.json
   def update
-    respond_to do |format|
-      if @admin_product.update(product_params)
-        redirect_to admin_product_path(@admin_product), notice: "Product was successfully updated."
-        format.json { render :show, status: :ok, location: @admin_product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
+    @admin_product = Product.find(params[:id])
+    if @admin_product.update(admin_product_params.reject { |k| k["images"] })
+      if admin_product_params["images"]
+         admin_product_params["images"].each do |image|
+          @admin_product.images.attach(image)
+        end
       end
+      redirect_to admin_products_path, notice: "Product updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -64,7 +66,7 @@ class Admin::ProductsController < AdminController
     end
 
     # Only allow a list of trusted parameters through.
-    def product_params
+    def admin_product_params
       params.require(:product).permit(:name, :description, :price, :category_id, :active, images: [])
     end
 end
