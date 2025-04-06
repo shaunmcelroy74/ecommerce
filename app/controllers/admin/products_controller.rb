903a2b6701c7300b0f327177a28b14f790072954
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-06 15:45:15 -0500

    Added an array to products_controller to add images to product page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -65,6 +65,6 @@ class Admin::ProductsController < AdminController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id, :active, :image)
+      params.require(:product).permit(:name, :description, :price, :category_id, :active, images: [])
     end
 end
