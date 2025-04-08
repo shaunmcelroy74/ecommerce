ba74a99561afc3210d016db867d4a954b96b4c71
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-08 11:28:30 -0500

    Created OrderProduct model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_07_164220) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_08_162557) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +58,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_07_164220) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "customer_email"
     t.boolean "fulfilled"
@@ -80,5 +90,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_07_164220) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
 end
