7198957094c4cdd434d640cec36944758297a394
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-08 13:14:58 -0500

    Added quick stats to admin_controller and displayed revenue in index.html.erb in admin view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,12 @@ class AdminController < ApplicationController
   layout "admin"
   before_action :authenticate_admin!
   def index
+    @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+    @quick_stats = {
+      sales: Order.where(created_at: Time.now.midnight..Time.now).count,
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(),
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(),
+      per_sale:  OrderProduct.joins(:order).where(orders: { created_at: Time.now.midnight...Time.now }).average(:quantity)
+    }
   end
 end
