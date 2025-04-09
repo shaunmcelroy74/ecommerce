b3a0800046d1236a45df104a4a92d35c3cba7d52
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-09 17:57:41 -0500

    Updated categories show.html.erb to display Category show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many_attached :images do |attachable|
   attachable.variant :thumb, resize_to_fill: [ 50, 50 ]
+  attachable.variant :medium, resize_to_fill: [ 250, 250 ]
 end
 
 
