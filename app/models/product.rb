fedb9caaf7e672a967003a38d9c810eb2a67cc5d
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-10 18:25:02 -0500

    Changed photo size in model product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many_attached :images do |attachable|
-    attachable.variant :thumb,  resize_to_fill: [ 50, 50 ]
+    attachable.variant :thumb,  resize_to_fill: [ 192, 192 ]
     attachable.variant :medium, resize_to_fill: [ 250, 250 ]
   end
 
