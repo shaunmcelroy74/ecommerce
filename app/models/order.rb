718d0c021a3d0ec03a3d12f9177d267b8f05b85a
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-11 09:00:12 -0500

    Removed cart validations and will go back to validate once checkout has been created

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_products, dependent: :destroy
 
-  validates :name,    presence: true
   validates :address, presence: true
-
-  validates :cart_data, presence: true
-  validate  :cart_data_must_be_valid_json
-
-  validate :must_have_at_least_one_product
 
   private
 
