ea073cf0f150c977994715e745bdc8030e6e45ae
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-15 12:26:52 -0500

    Created pages_controller under controller/admin

---
+++ app/controllers/admin/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def about
+    @page = Page.find_by!(title: "About")
+  end
+
+  def contact
+    @page = Page.find_by!(title: "Contact")
+  end
+end
