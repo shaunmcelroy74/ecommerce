943b048035f535847b8be21d088f00ae085d4bc2
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-23 11:57:11 -0500

    Added belongs_to :province, optional: true to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_23_164544) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_23_165402) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -110,7 +110,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_164544) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "province_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -119,4 +121,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_164544) do
   add_foreign_key "order_products", "orders"
   add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "users", "provinces"
 end
