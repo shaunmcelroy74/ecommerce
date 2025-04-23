498a6a4237d1a6324f123aee69e7380d5f7181d7
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-23 11:46:38 -0500

    Added a Province model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_23_164033) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_23_164544) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -93,6 +93,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_164033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
