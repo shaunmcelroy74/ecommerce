03a18a55b1dd5d8e53032c764b125056022f079f
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-04-23 12:00:47 -0500

    Permitted devise params in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # Only allow modern browsers supporting webp, web push, badges, import maps, CSS nesting, and CSS :has
   allow_browser versions: :modern
+
+  # Devise: permit extra fields on sign up & account update
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attrs = %i[first_name last_name street city postal_code province_id]
+    devise_parameter_sanitizer.permit(:sign_up,        keys: attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: attrs)
+  end
 end
